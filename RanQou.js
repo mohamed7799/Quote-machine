4dde7281337c12958d5fffc2b643fef815367a1c
mohamed7799 <mohmedamged9@gmail.com>
2019-08-29 16:33:12 +0200

    fixed the api and used fetch instead of xhr

--- RanQou.js
+++ RanQou.js
@@ -1,49 +1,37 @@
 //variables
-let New=document.getElementById("new");
-let quote=document.getElementById("quote");
-let author=document.getElementById("author");
-let twitter=document.getElementById("twit");
+let New = document.getElementById("new");
+let quote = document.getElementById("quote");
+let author = document.getElementById("author");
+let twitter = document.getElementById("twit");
 let data;
 
 //functions
 
-function new_quote(){
-	
-	let request= new XMLHttpRequest();
-	//set the request
-	request.open('GET','https://talaikis.com/api/quotes/random/');
-	//when the request get loaded
-	request.onload=function(){
-
-		data=JSON.parse(request.responseText);
-		quote.innerHTML=`"${data.quote}"`;
-		author.innerHTML=`- ${data.author}`;
-	};
-	//send the request
-	request.send();
+function new_quote() {
+  fetch("https://api.quotable.io/random")
+    .then(res => res.json())
+    .then(body => {
+      quote.innerHTML = `"${body.content}"`;
+      author.innerHTML = `- ${body.author}`;
+    })
+    .catch(error => alert(`Something went wrong the error is ${error}`));
 }
 
-function Twit(){
-
-	if(data.quote.length>140){
-		alert("The Quote IS Too Big");
-	}
-	else
-	{
-	let twtLink = 'https://twitter.com/intent/tweet?text=' +data.quote;
- 	window.open(twtLink);
-	}
-
+function Twit() {
+  if (data.quote.length > 140) {
+    alert("The Quote IS Too Big");
+  } else {
+    let twtLink = "https://twitter.com/intent/tweet?text=" + data.quote;
+    window.open(twtLink);
+  }
 }
-
 
 //eventListners
 
-New.addEventListener("click",new_quote);
+New.addEventListener("click", new_quote);
 
-twitter.addEventListener("click",Twit);
+twitter.addEventListener("click", Twit);
 
 //main
 
 new_quote();
-
